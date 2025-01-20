6ea37d55066469c8c1514fc3e36931ca2f075bb1
AlexisByte <153880293+AlexisByte@users.noreply.github.com>
2025-01-20 13:23:33 -0500

    frontend avance
    frontend avance

---
+++ frontend/src/app/Template/inicioportada/inicioportada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicioportada',
+  standalone: true,
+  imports: [],
+  templateUrl: './inicioportada.component.html',
+  styleUrl: './inicioportada.component.css'
+})
+export class InicioportadaComponent {
+
+}
