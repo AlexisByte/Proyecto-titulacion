d64aca890ea84cfe99e12f8d614c81365c352320
AlexisByte <153880293+AlexisByte@users.noreply.github.com>
2025-07-01 13:35:17 -0500

    act prod

--- Backend/sync.js
+++ Backend/sync.js
@@ -1,12 +1,18 @@
+const env = process.env.NODE_ENV || 'production';
+const config = require(__dirname + '/config/config.json')[env];
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 const db = require('./models');
 
 (async () => {
   try {
-    await db.sequelize.sync({ force: true }); // Cambiar a `true` si quieres forzar la recreación
+    await db.sequelize.sync({ force: true });
     console.log("Database & tables created!");
   } catch (error) {
     console.error("Error during database sync:", error);
   } finally {
-    process.exit(); // Cerrar el proceso después de la sincronización
+    process.exit();
   }
 })();
